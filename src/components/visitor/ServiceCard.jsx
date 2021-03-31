a9a2b36297611c420bec0446aa1fd0d0faf922d3
nightmre789 <nightmre789@gmail.com>
2021-04-01 03:04:05 +0500

    mobile optimization

--- src/components/visitor/ServiceCard.jsx
+++ src/components/visitor/ServiceCard.jsx
@@ -5,10 +5,10 @@ import SVG from "react-inlinesvg";
 export default ({ src, title, text }) => {
    return (
       <div className="flex flex-col items-center p-4 text-center text-gray-800 duration-100 transform rounded-lg cursor-default hover:shadow-lg hover:bg-white hover:bg-opacity-80">
-         <div className="flex items-center mt-4 gap-x-4">
+         <div className="flex items-center mt-4">
             <SVG
                src={src}
-               className="flex items-center transform scale-150 fill-current justfiy-center"
+               className="flex items-center mr-4 transform scale-150 fill-current justfiy-center"
             />
             <h1 className="mt-1 text-3xl font-bold font-edmond">{title}</h1>
          </div>
